9bfad8f7d04ec0b0e74d7ac67b953686dfd40ff8
Valeria Moreno <a01280603@itesm.mx>
2022-12-15 15:00:42 +0200

    Filter test update

--- test/filter.test.js
+++ test/filter.test.js
@@ -10,16 +10,16 @@ const users = [
     
 describe("Filter", () => {
  it("Test with empty", () =>{
-     expect(filter()).to.equal([[]])
+     expect(filter()).to.eql([[]])
  });
  it("Test with correct function", () =>{
-     expect(filter(users,({active}) => active)).to.equal([ { user: 'barney', active: true } ])
+     expect(filter(users,({active}) => active)).to.eql([ { user: 'barney', active: true } ])
  });
  it("Test with correct array", () =>{
-    expect(filter(users,({ user }) => user)).to.equal([ { user: 'barney', active: true },{"active": false, "user": "fred"} ])
+    expect(filter(users,({ user }) => user)).to.eql([ { user: 'barney', active: true },{"active": false, "user": "fred"} ])
  });
  it("Test with incorrect function", () =>{
-    expect(filter(users,({ user }) => 'barney')).to.equal([ { user: 'barney', active: true } ])
+    expect(filter(users,({ user }) => 'barney')).to.eql([ { user: 'barney', active: true } ])
  });
  
 })
